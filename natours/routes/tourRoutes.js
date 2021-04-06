0368599723f741c227565960cd998cf550e1a3f2
Dustin Heaps <dustinheaps89@gmail.com>
2021-04-06 14:43:28 -0400

    completed first part (express) of natours app

---
+++ natours/routes/tourRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+  checkID,
+  checkBody
+} = require('../controllers/tourController');
+
+const router = express.Router();
+
+router.param('id', checkID)
+// router.param('body', checkBody)
+
+router
+  .route('/')
+  .get(getAllTours)
+  .post(checkBody, createTour);
+
+router
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+module.exports = router;
